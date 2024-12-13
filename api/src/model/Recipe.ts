609a79f7b23d210c374dd73bd78465f0e4a60392
AngryAndew <andrew.taylor.4378@gmail.com>
2024-12-12 23:28:51 -0500

    added put recipe api

---
+++ api/src/model/Recipe.ts
@@ -0,0 +1,8 @@
+export type RecipeId = string;
+export type UserId = string;
+
+export type Recipe = {
+    recipeId: RecipeId,
+    userId: UserId,
+    description: string
+}
--- cdk/lib/construct/Lambda.ts
+++ cdk/lib/construct/Lambda.ts
@@ -1,8 +1,9 @@
-import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Code, Function, FunctionUrlAuthType, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 
 export type ApiLambdaProps = {
     apiPath: string,
+    authEnabled: boolean
 };
 
 export class ApiLambda extends Construct {
@@ -10,14 +11,19 @@ export class ApiLambda extends Construct {
     constructor(scope: Construct, id: string, props: ApiLambdaProps) {
         super(scope, id);
 
-        this.lambdaFunction = this.createlambdaFunction(props.apiPath);
+        this.lambdaFunction = this.createlambdaFunction(props.apiPath, props.authEnabled);
     }
-    private createlambdaFunction(apiPath: string): Function {
-        return new Function(this, 'Function', {
+    private createlambdaFunction(apiPath: string, authEnabled: boolean): Function {
+        const func = new Function(this, 'Function', {
             runtime: Runtime.NODEJS_LATEST,
             handler: `dist/index.${apiPath}`,
             code: Code.fromAsset('../api/build/lambda.zip'),
+        });
+        func.addFunctionUrl({
+            authType: authEnabled ? FunctionUrlAuthType.AWS_IAM : FunctionUrlAuthType.NONE
         })
+
+        return func;
     }
 }
 
